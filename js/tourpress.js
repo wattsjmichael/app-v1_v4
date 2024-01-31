e5d24eae7b6920e0014d08af9fa403a3a65a8847
Michael Watts <wattsjmichael@gmail.com>
2024-01-31 09:03:36 -0800

    wrong place

--- js/tourpress.js
+++ js/tourpress.js
@@ -144,7 +144,7 @@ window.addEventListener('orientationchange', ochange);
 // console.log(tenant);
 embedpano({
    
-    xml: (tenant) ? tenant + ".xml" : "default.xml",
+    xml: (tenant) ? tenant + ".xml" : "all.xml",
     initvars: {
         maps:'./maps/',
         skin: 'ak',
